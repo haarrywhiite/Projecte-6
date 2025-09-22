9cd9c6fcff293b9cf2bd3b7d00c68bd666242998
alum11 alum11 <alum11@ASIX1.LAN>
2025-09-22 10:26:09 +0200

    ACT4

--- RA1_1.2/RA1_1.2_7_HarryWhite.py
+++ RA1_1.2/RA1_1.2_7_HarryWhite.py
@@ -5,10 +5,10 @@
 # Aquest programa funciona correctament, però volem moure la definició de la funció al
 # final. Ho pots fer sense que doni error?
 
-saluda()
-saluda()
 
 def saluda():
     print("Hola!")
+    
+saluda()
 
 
