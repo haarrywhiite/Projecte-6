a895ab653cd9baea87d1866b34603b152a8b9629
haarrywhiite <haarrywhiite@gmail.com>
2025-09-22 10:14:35 +0200

    Update RA1_1.2_PLANTILLA.py

---
+++ RA1_1.2/RA1_1.2_PLANTILLA.py
@@ -0,0 +1,11 @@
+# Autor: Harry White
+# Fecha: 2024-06-22
+
+
+# Descripció del programa o enunciat de l'exercici:
+
+# Especificacions d'entrada
+
+# Especificacions de sortida
+
+#Commentaris addicionals sobre el codi i el programa
