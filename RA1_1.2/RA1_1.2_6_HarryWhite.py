72e8ba0079b9dd44586a7692df4e747da60b323f
haarrywhiite <haarrywhiite@gmail.com>
2025-09-22 10:12:21 +0200

    Update RA1_1.2_6_HarryWhite.py

--- RA1_1.2/RA1_1.2_6_HarryWhite.py
+++ RA1_1.2/RA1_1.2_6_HarryWhite.py
@@ -2,17 +2,12 @@
 # Fecha: 2024-06-22
 
 
-# Constant
-PI = 3.1416
+# Aquest codi no funciona. Troba i corregeix l’error:
 
-# Funció
-def calcular_perimetre(radi):
-	return 2 * PI * radi
+def multiplica(a, b):
+	return a * b
 
-# Entrada de l'usuari
-radi = float(input("Introdueix el radi del cercle: "))
+resultat = multiplica(3, 4)
+print (resultat)
 
-# Sortida per pantalla
-perimetre = calcular_perimetre(radi)
-print(f"El perímetre del cercle és: {perimetre}")
-
+# No funcionava perque es multiplica no multiplicar
