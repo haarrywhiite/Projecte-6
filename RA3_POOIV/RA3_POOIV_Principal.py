355c1e659783ec5d6d6a017213af68757a3c36b7
alum11 alum11 <alum11@ASIX1.LAN>
2026-02-17 09:07:27 +0100

    POOIV

--- RA3_POOIV/RA3_POOIV_Principal.py
+++ RA3_POOIV/RA3_POOIV_Principal.py
@@ -1,6 +1,73 @@
+# Autor: Harry White
+# Data: 28-1-26
+
+#HERÈNCIA
+#EX1
+from Animals import Animal, Gos, Gat
+
+a = Animal()
+g = Gos()
+c = Gat()
+
+a.parlar()
+g.parlar()
+c.parlar()
+
+#EX2
+from Vehicles import Cotxe
+
+marca_cotxe = "Toyota"
+cotxe = Cotxe(marca_cotxe)
+cotxe.arrencar()
+cotxe.tocar_claxon()
+
+#EX3
+from Persones_treballadors import Persona, Treballador
+
+persona = Persona("Harry", 23)
+persona.saludar()
+treballador = Treballador("Gerard", 22, "Programador")
+treballador.saludar()
+treballador.mostrar_feina()
+
+#EX4
+from Figura_geomètrica import Figura, Quadrat, Cercle
+figura = Figura()
+figura.area()
+quadrat = Quadrat(4)
+print(f"Àrea del quadrat: {quadrat.area()}")
+cercle = Cercle(3)
+print(f"Àrea del cercle: {cercle.area()}")
+
+#EX5
+from Biblioteca import LlibrePaper, LlibreDigital
+llibre_paper = LlibrePaper("El Quijote", "Miguel de Cervantes", 863)
+llibre_digital = LlibreDigital("1984", "George Orwell", "ePub")
+print(llibre_paper.mostrar_info())
+print(llibre_digital.mostrar_info())
+
 from Empleat import Empleat, Fixe, Autonom
 from Missatger import Missatger, Email, SMS, WhatsApp
 from Vehicle import Vehicle, Cotxe, Bicicleta, Barca
+
+#POLIMORFISME
+#EX1
+from Sons_animals import Animal, Gat, Vaca, reproduir_soroll
+
+gat = Gat()
+vaca = Vaca()
+reproduir_soroll(gat)
+reproduir_soroll(vaca)
+
+#EX2
+
+from Dibuixar_Figures import Figura, Cercle, Quadrat, Triangle
+
+figures = [Figura(), Cercle(), Quadrat(), Triangle()]
+for f in figures:
+    f.dibuixar()
+
+
 
 # Exercici 3
 def mostrar_sous(llista_empleats):
